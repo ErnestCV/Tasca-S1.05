027f55f723108e96ea0470757c1e3d7042e8c291
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-06-22 22:26:19 +0200

    Versió revisada

--- n1ex1/src/main/Test/DirectorisTest.java
+++ n1ex1/src/main/Test/DirectorisTest.java
@@ -3,6 +3,9 @@ import static org.assertj.core.api.Assertions.*;
 import n1ex1.GestioDirectori;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.util.List;
+
 public class DirectorisTest {
 
     @Test
@@ -10,8 +13,11 @@ public class DirectorisTest {
 
         GestioDirectori gestioDirectori = new GestioDirectori();
 
-        assertThat(gestioDirectori.mostraDirectoriAlfabeticJava("C:\\Users\\ecv-s\\IdeaProjects")
-                .equals(gestioDirectori.mostraDirectoriAlfabeticApache("C:\\Users\\ecv-s\\IdeaProjects")));
+        List<File> directorisListJava = gestioDirectori.mostraDirectoriAlfabeticJava("C:\\Users\\ecv-s\\IdeaProjects");
+        List<File> directorisListApache = gestioDirectori.mostraDirectoriAlfabeticApache("C:\\Users\\ecv-s\\IdeaProjects");
+        directorisListApache.remove(0); //Hem d'eliminar el primer element, el directori parent
+
+        assertThat(directorisListJava.equals(directorisListApache)).isTrue();
 
     }
 
